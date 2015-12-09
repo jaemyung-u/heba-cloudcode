27d50296b971a8d98592b80c0e57f6e546a44ae1
Jaemyung Yu <jaemyung.u@gmail.com>
2015-12-10 03:12:25 +0900

    simplify

--- cloud/main.js
+++ cloud/main.js
@@ -1,28 +1,3 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
-
-Parse.Cloud.define("averageLocation", function(request, response) {
-  var query = new Parse.Query("Stamp");
-  query.descending("updatedAt");
-  query.limit(5);
-  query.find({
-    success: function(results) {
-      var sum = "";
-      for (var i = 0; i < results.length; ++i) {
-        sum += results[i].get("comment");
-      }
-      response.success(sum);
-    },
-    error: function() {
-      response.error("comment summation failed.");
-    }
-  });
-});
-
 Parse.Cloud.afterSave("Stamp", function(request) {
   query = new Parse.Query("Event");
   query.get(request.object.get("eventId"), {
@@ -39,5 +14,4 @@ Parse.Cloud.afterSave("Stamp", function(request) {
     }
   });
 });
-
 
